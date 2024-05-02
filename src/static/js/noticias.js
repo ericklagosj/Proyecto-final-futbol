551e82441fa19bc37eb69b5babcfb678e1c5a7b1
burgoshk <burgosgmx@gmail.com>
2024-05-02 13:06:34 -0400

    navbar agregado a seccion noticias, index modificado.

--- src/static/js/noticias.js
+++ src/static/js/noticias.js
@@ -1,21 +1,35 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
+  toggle.addEventListener("click", () => {
+    // Add show-menu class to nav menu
+    nav.classList.toggle("show-menu");
 
-const btnHam = document.querySelector('.ham-btn');
-const btnTimes = document.querySelector('.times-btn');
-const navBar = document.getElementById('nav-bar');
+    // Add show-icon to show and hide the menu icon
+    toggle.classList.toggle("show-icon");
+  });
+};
 
-btnHam.addEventListener('click', function(){
-    if(btnHam.className !== ""){
-        btnHam.style.display = "none";
-        btnTimes.style.display = "block";
-        navBar.classList.add("show-nav");
-    }
-})
+showMenu("nav-toggle", "nav-menu");
 
-btnTimes.addEventListener('click', function(){
-    if(btnHam.className !== ""){
-        this.style.display = "none";
-        btnHam.style.display = "block";
-        navBar.classList.remove("show-nav");
-    }
-})
+const btnHam = document.querySelector(".ham-btn");
+const btnTimes = document.querySelector(".times-btn");
+const navBar = document.getElementById("nav-bar");
+
+btnHam.addEventListener("click", function () {
+  if (btnHam.className !== "") {
+    btnHam.style.display = "none";
+    btnTimes.style.display = "block";
+    navBar.classList.add("show-nav");
+  }
+});
+
+btnTimes.addEventListener("click", function () {
+  if (btnHam.className !== "") {
+    this.style.display = "none";
+    btnHam.style.display = "block";
+    navBar.classList.remove("show-nav");
+  }
+});
