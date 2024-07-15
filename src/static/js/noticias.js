98c572d971d10d0be29fe97992e5fb2159513c8d
burgoshk <burgosgmx@gmail.com>
2024-07-15 14:28:42 -0400

    Solucion de bugs, ahora hay mas bugs

--- src/static/js/noticias.js
+++ src/static/js/noticias.js
@@ -1,35 +1,72 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  // Smooth Scroll to Top
-  document
-    .querySelector(".footer-top-link a")
-    .addEventListener("click", function (event) {
-      event.preventDefault();
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    });
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-  // Toggle mobile menu
-  document.getElementById("mobile-menu").addEventListener("click", function () {
-    document.getElementById("navbar-links").classList.toggle("active");
-    document.getElementById("navbar-actions").classList.toggle("active");
-    document.getElementById("navbar-search").classList.toggle("active");
+  toggle.addEventListener("click", () => {
+    // Add show-menu class to nav menu
+    nav.classList.toggle("show-menu");
+    // Add show-icon to show and hide menu icon
+    toggle.classList.toggle("show-icon");
   });
+};
 
-  // Form Submission (Example)
-  document
-    .querySelector("#subscribe-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+showMenu("nav-toggle", "nav-menu");
 
-      // Simulate form submission
-      const email = document.querySelector("#subscribe-form input").value;
-      if (email) {
-        alert(`Suscripción exitosa con el email: ${email}`);
-        // Here you would typically send the email to your server
-        document.querySelector("#subscribe-form").reset();
-      } else {
-        alert("Por favor, ingrese un email válido.");
-      }
-    });
+/*=============== SHOW DROPDOWN MENU ===============*/
+const dropdownItems = document.querySelectorAll(".dropdown__item");
+
+// 1. Select each dropdown item
+dropdownItems.forEach((item) => {
+  const dropdownButton = item.querySelector(".dropdown__button");
+
+  // 2. Select each button click
+  dropdownButton.addEventListener("click", () => {
+    // 7. Select the current show-dropdown class
+    const showDropdown = document.querySelector(".show-dropdown");
+
+    // 5. Call the toggleItem function
+    toggleItem(item);
+
+    // 8. Remove the show-dropdown class from other items
+    if (showDropdown && showDropdown !== item) {
+      toggleItem(showDropdown);
+    }
+  });
 });
 
-/* FIN SCRIPT NAVBAR */
+// 3. Create a function to display the dropdown
+const toggleItem = (item) => {
+  // 3.1. Select each dropdown content
+  const dropdownContainer = item.querySelector(".dropdown__container");
+
+  // 6. If the same item contains the show-dropdown class, remove
+  if (item.classList.contains("show-dropdown")) {
+    dropdownContainer.removeAttribute("style");
+    item.classList.remove("show-dropdown");
+  } else {
+    // 4. Add the maximum height to the dropdown content and add the show-dropdown class
+    dropdownContainer.style.height = dropdownContainer.scrollHeight + "px";
+    item.classList.add("show-dropdown");
+  }
+};
+
+/*=============== DELETE DROPDOWN STYLES ===============*/
+const mediaQuery = matchMedia("(min-width: 1118px)"),
+  dropdownContainer = document.querySelectorAll(".dropdown__container");
+
+// Function to remove dropdown styles in mobile mode when browser resizes
+const removeStyle = () => {
+  // Validate if the media query reaches 1118px
+  if (mediaQuery.matches) {
+    // Remove the dropdown container height style
+    dropdownContainer.forEach((e) => {
+      e.removeAttribute("style");
+    });
+
+    // Remove the show-dropdown class from dropdown item
+    dropdownItems.forEach((e) => {
+      e.classList.remove("show-dropdown");
+    });
+  }
+};
